78bdd4af39e64c322d76a1b01ad9b8f9986e617d
Deaf <de>
2024-06-11 14:11:09 +0800

    test10

--- src/test10.java
+++ src/test10.java
@@ -1,7 +1,11 @@
 public class test10 {
 
     public static void main(String[] args) {
-        System.out.println("专业实习");
+        for(int i=0;i<3;i++)
+        {
+            System.out.println("专业实习"+i);
+        }
+
     }
 }
 
